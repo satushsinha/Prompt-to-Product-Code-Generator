f825b4b32ee74dec88ff224928370b781b2913b5
satushsinha <ayushiiitbbsr@gmail.com>
2025-10-10 00:04:34 +0530

    added planner and architect node

--- agent/graph.py
+++ agent/graph.py
@@ -1,26 +1,55 @@
 from langchain_groq import ChatGroq
 
 from dotenv import load_dotenv
+from prompts import *
+from states import *
 load_dotenv()
 
+from langgraph.graph import StateGraph
+from langgraph.constants import END
+from langchain.globals import set_verbose, set_debug
+
+
+set_debug(True)
+set_verbose(True)
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
 #resp = llm.invoke("What is the today's date")
 
-from pydantic import BaseModel
+def planner_agent(state: dict) -> dict:
+    """Converts user prompt into a structured Plan."""
+    user_prompt = state["user_prompt"]
+    resp = llm.with_structured_output(Plan).invoke(
+        planner_prompt(user_prompt)
+    )
+    if resp is None:
+        raise ValueError("Planner did not return a valid response.")
+    return {"plan": resp}
 
 
-user_prompt = "Create a simple calculator web application"
+def architect_agent(state: dict) -> dict:
+    """Creates TaskPlan from Plan."""
+    plan: Plan = state["plan"]
+    resp = llm.with_structured_output(TaskPlan).invoke(
+        architect_prompt(plan=plan.model_dump_json())
+    )
+    if resp is None:
+        raise ValueError("Planner did not return a valid response.")
 
-prompt = f"""
-You are the PLANNER agent. Convert the user prompt into a COMPLETE engineering project plan
+    resp.plan = plan
+    print(resp.model_dump_json())
+    return {"task_plan": resp}
 
-User request: {user_prompt}
-"""
+graph = StateGraph(dict)
+graph.add_node("planner", planner_agent)
+graph.add_node("architect", architect_agent)
+graph.add_edge("planner", "architect")
+graph.set_entry_point("planner")
 
-class Plan(BaseModel):
-  pass
+agent = graph.compile()
 
-resp = llm.with_structured_output(Plan).invoke(prompt)
-print(resp)
+if __name__ == "__main__":
+  user_prompt = "Create a simple calculator web application"
+  result = agent.invoke({"user_prompt": user_prompt})
+  print(result)
